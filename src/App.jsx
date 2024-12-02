82a643711c34e39054f9ad233c5c624675ae5152
EvaneRex <144788295+EvaneRex@users.noreply.github.com>
2024-12-02 11:34:07 +0100

    styled the bg and fixed the size of the tree for the time. Working on the snow portion

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,6 @@
 import gsap from "gsap";
 import Tree from "./components/tree_star";
 
-gsap.to(".logo", {
-  duration: 2,
-  x: 300,
-  backgroundColor: "#560563",
-  borderRadius: "20%",
-  border: "5px solid white",
-}); //called a tween
 function App() {
   return (
     <>
