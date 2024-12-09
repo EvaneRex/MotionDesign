439ee378b7ea9a63f6f2edd47425088cf7832239
EvaneRex <144788295+EvaneRex@users.noreply.github.com>
2024-12-09 10:41:30 +0100

    having trouble with intergrating the scrolltrigger, migth add it in a useeffect instead and see how that goes

--- src/components/newYearGreeting.jsx
+++ src/components/newYearGreeting.jsx
@@ -1,12 +1,16 @@
 import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
 import { useRef } from "react";
+import ScrollTrigger from "gsap/ScrollTrigger";
+
+// Register the plugin
+gsap.registerPlugin(ScrollTrigger);
 
 export default function NewYearGreeting(isVisible) {
   const NYRef = useRef(null);
 
   useGSAP(() => {
-    if (isVisible) {
+    if (NYRef.current && isVisible) {
       gsap.fromTo(
         NYRef.current,
         { opacity: 0, y: "100vh", x: 0, scale: 0 },
@@ -17,6 +21,12 @@ export default function NewYearGreeting(isVisible) {
           duration: 5,
           ease: "back.inOut",
           scale: 1,
+          scrollTrigger: {
+            trigger: NYRef.current,
+            start: "top 80%",
+            end: "bottom 10%",
+            scrub: true,
+          },
         }
       );
     }
