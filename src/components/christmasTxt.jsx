34100530f86ae7cb36817ec7fb0c15cfd75e200b
EvaneRex <144788295+EvaneRex@users.noreply.github.com>
2024-12-09 11:32:02 +0100

    remmoved the isVisble from the christmasTxt component as its not used

--- src/components/christmasTxt.jsx
+++ src/components/christmasTxt.jsx
@@ -2,18 +2,18 @@ import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
 import { useRef } from "react";
 
-export default function ChristmasGreeting(isVisible) {
+export default function ChristmasGreeting() {
   const textRef = useRef(null);
 
   useGSAP(() => {
-    if (isVisible) {
+    if (textRef.current) {
       gsap.fromTo(
         textRef.current,
         { opacity: 0, y: 0, x: 0, scale: 0 },
         { opacity: 1, y: 150, x: 0, duration: 5, ease: "back.inOut", scale: 1 }
       );
     }
-  }, [isVisible]);
+  }, []);
 
   return <h1 ref={textRef}>Merry Christmas</h1>;
 }
