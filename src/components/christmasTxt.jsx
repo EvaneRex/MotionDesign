179674d85c99dc157418241ed3bc67409d171d7a
EvaneRex <144788295+EvaneRex@users.noreply.github.com>
2024-12-09 10:28:35 +0100

    changed the y part of the texts animation, working on adding a scrolltrigger for the newyears and maybe changing the animation to be more explosive some how

--- src/components/christmasTxt.jsx
+++ src/components/christmasTxt.jsx
@@ -10,7 +10,7 @@ export default function ChristmasGreeting(isVisible) {
       gsap.fromTo(
         textRef.current,
         { opacity: 0, y: 0, x: 0, scale: 0 },
-        { opacity: 1, y: 50, x: 0, duration: 5, ease: "back.inOut", scale: 1 }
+        { opacity: 1, y: 150, x: 0, duration: 5, ease: "back.inOut", scale: 1 }
       );
     }
   }, [isVisible]);
--- src/components/newYearGreeting.jsx
+++ src/components/newYearGreeting.jsx
@@ -1,3 +1,25 @@
-export default function NewYearGreeting() {
-  return <h2>Happy New Year!</h2>;
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import { useRef } from "react";
+
+export default function NewYearGreeting(isVisible) {
+  const NYRef = useRef(null);
+
+  useGSAP(() => {
+    if (isVisible) {
+      gsap.fromTo(
+        NYRef.current,
+        { opacity: 0, y: "100vh", x: 0, scale: 0 },
+        {
+          opacity: 1,
+          y: 150,
+          x: 0,
+          duration: 5,
+          ease: "back.inOut",
+          scale: 1,
+        }
+      );
+    }
+  }, [isVisible]);
+  return <h2 ref={NYRef}>Happy New Year!</h2>;
 }
