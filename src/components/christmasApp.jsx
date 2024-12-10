cad78b01f04ecaceabf2f99ed4b950b68ca71c56
EvaneRex <144788295+EvaneRex@users.noreply.github.com>
2024-12-10 08:42:48 +0100

    made it so the text reappears when scrolling between the to thing.. might make this a thing in the app since it affects both

--- src/components/christmasApp.jsx
+++ src/components/christmasApp.jsx
@@ -2,11 +2,40 @@ import ChristmasGreeting from "./christmasTxt";
 import Landscape from "./landscape";
 import Snow from "./snow";
 import Tree from "./tree_star";
+import gsap from "gsap";
+import ScrollTrigger from "gsap/ScrollTrigger";
+import { useEffect, useState } from "react";
+
+// Register plugin
+gsap.registerPlugin(ScrollTrigger);
 
 function ChristmasApp() {
+  const [isVisible, setVisible] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const newYearElement = document.querySelector(".christmas");
+      if (newYearElement) {
+        const rect = newYearElement.getBoundingClientRect();
+        if (rect.top < window.innerHeight && rect.bottom > 0) {
+          setVisible(true);
+        } else {
+          setVisible(false);
+        }
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    handleScroll();
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
     <div className="christmas">
-      <ChristmasGreeting />
+      <ChristmasGreeting isVisible={isVisible} />
       <Tree />
       <Snow />
       <Landscape />
--- src/components/christmasTxt.jsx
+++ src/components/christmasTxt.jsx
@@ -2,18 +2,25 @@ import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
 import { useRef } from "react";
 
-export default function ChristmasGreeting() {
+export default function ChristmasGreeting({ isVisible }) {
   const textRef = useRef(null);
 
   useGSAP(() => {
-    if (textRef.current) {
+    if (textRef.current && isVisible) {
       gsap.fromTo(
         textRef.current,
         { opacity: 0, y: 0, x: 0, scale: 0 },
-        { opacity: 1, y: 150, x: 0, duration: 5, ease: "back.inOut", scale: 1 }
+        {
+          opacity: 1,
+          y: 150,
+          x: 0,
+          duration: 5,
+          ease: "power4.inOut",
+          scale: 1,
+        }
       );
     }
-  }, []);
+  }, [isVisible]);
 
   return <h1 ref={textRef}>Merry Christmas</h1>;
 }
